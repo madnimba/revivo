9b43147225903eeb510683831e21a3dd6ea9e9ec
madnimba <jarintasneem248@gmail.com>
2023-08-13 18:28:47 +0600

    Initialize: server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+app.get(api+'/product',(req,res)=>{
+    const product =
+    {
+        id:1,
+        name:'shoe'
+    }
+    res.send(product);
+})
+
+app.listen(3000,()=>
+{
+    console.log('server is running');
+})
