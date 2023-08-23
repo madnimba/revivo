0414637b8c0977f87b818f6b7f970672ad826ea1
Saba <121762847+SabaJhim@users.noreply.github.com>
2023-08-23 22:41:40 +0600

    login sql added

---
+++ Database/login.js
@@ -0,0 +1,40 @@
+const database = require('./main_db');
+
+
+async function readEmail(mail,role){
+    let sql="";
+    if(role=='user'){
+         sql = `
+        SELECT 
+            *
+        FROM 
+            Basic_user
+        WHERE 
+            E_mail = :mail
+        `;
+        
+    }
+    else if(role=='shop'){
+         sql = `
+        SELECT 
+            *
+        FROM 
+            Shop
+        WHERE 
+            E_mail = :mail
+        `;
+    }
+ 
+    const binds = {
+        mail: mail
+    }
+    const resul= (await database.execute(sql, binds, database.options));
+    return resul.rows;
+}
+
+
+
+
+module.exports={
+    readEmail
+}
