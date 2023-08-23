bca0eb3bdea9ba0cbb640273f25576f2b571f9ca
Saba <121762847+SabaJhim@users.noreply.github.com>
2023-08-23 22:42:10 +0600

    register sql added

---
+++ Database/register.js
@@ -0,0 +1,41 @@
+const database = require('./main_db');
+
+
+
+async function createNewUser(user){
+    const sql = `
+        INSERT INTO
+            BASIC_USER(First_name,Last_name,E_mail,Address,Password,Phone)
+        VALUES 
+            (:fname,:lname,:email,:address,:pass,:phone)`
+    const binds = {
+        fname: user.fname,
+        lname:user.lname,
+        email :user.email,
+        address: user.address,
+        pass: user.pass,
+        phone: user.phone,
+    }
+    return await database.execute(sql, binds, {});
+}
+
+async function createNewShopUser(shop){
+    const sql = `
+        INSERT INTO
+            SHOP(Name,E_mail,Password,Phone)
+        VALUES 
+            (:name,:email,:pass,:phone)`
+            
+    const binds = {
+        name: user.name,
+        email :user.email,
+        pass: user.pass,
+        phone: user.phone,
+    }
+    return await database.execute(sql, binds, {});
+}
+
+module.exports={
+    createNewUser,
+    createNewShopUser,
+}
