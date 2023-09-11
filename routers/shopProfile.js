f15621c4e6e68f86ddccfac4dad66fce25e6fa05
SabaJhim <mubashwerafairuz@gmail.com>
2023-09-11 11:38:12 +0600

    user profile

--- routers/shopProfile.js
+++ routers/shopProfile.js
@@ -6,7 +6,11 @@ const DB_shop=require('../Database/shop');
 
 router.get('/',async(req,res)=>{
     const shop=await DB_shop.getShopByID(req.user.id);
-    res.render('realShopProfile.ejs',{shop:shop});
+    let orders=await DB_shop.getProcessedOrders(req.user.id);
+    console.log(orders);
+   
+
+        res.render('realShopProfile.ejs',{shop:shop,product:orders});
 })
 
 module.exports=router;
